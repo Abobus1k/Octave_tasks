1462db792bd9df3a9e230a0f4da56cdceb882e85
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2023-05-29 20:18:08 +0300

    Update mayatnik.m

--- mayatnik.m
+++ mayatnik.m
@@ -1,4 +1,4 @@
-function [t, theta] = hometask(L, a, b, theta0, omega0, tspan)
+function [t, theta] = mayatnik(L, a, b, theta0, omega0, tspan)
 y0 = [theta0; omega0];
 odefun = @(t, y) [y(2); -9.81/L*sin(y(1))];
 [t, Y] = ode45(odefun, tspan, y0);
